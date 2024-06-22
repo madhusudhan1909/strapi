c6d64ed29b7c9b77b7b876bf787dffc52463d7e0
madhusudhan1909 <124492728+madhusudhan1909@users.noreply.github.com>
2024-06-22 16:21:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,6 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-variable "private_key" {
-  description = "The private key for SSH access"
-  type        = string
-  sensitive   = true
-}
-
 data "aws_instance" "existing_instance" {
   instance_id = "i-071170ba4826f6150"  // Specify the existing instance ID
 }
@@ -17,14 +11,13 @@ resource "null_resource" "provision_commands" {
       "sudo apt-get update",
       "sudo apt-get install -y npm",
       "sudo npm install pm2 -g",
-      "cd /home/ubuntu/strapi/examples/getstarted",
       "npm install",
-      "pm2 start npm --name 'strapi' -- start"
     ]
 
     connection {
       type        = "ssh"
-      user        = "ubuntu"
+      user        = "ubuntu" 
+      private_key = var.private_key
       host        = "13.229.80.114"  // Update with your existing instance's public IP
     }
   }
