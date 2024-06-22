23a508399fdabe38cb217d37e932b68f584d12ed
madhusudhan1909 <124492728+madhusudhan1909@users.noreply.github.com>
2024-06-22 15:43:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "strapi_instance" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file(var.pem_key_path)  // Update with correct path to your .pem file
+      private_key = var.private_key
       host        = "13.229.80.114"
     }
   }
